d1d9bdaa5ee485f95910b550af133c8391214190
Saulo Sousa <saulo_mg_tp@hotmail.com>
2023-06-21 15:35:35 -0300

    Foram criadas exceções no Tabuleiro para que não aja peças no mesmo lugar, e movimentações para o mesmo local na matriz

--- src/boardgame/Tabuleiro.java
+++ src/boardgame/Tabuleiro.java
@@ -7,37 +7,51 @@ public class Tabuleiro {
     private Piece[][] pieces;
 
     public Tabuleiro(int linhas, int colunas) {
-        this.linhas = linhas;
-        this.colunas = colunas;
-        pieces = new Piece[linhas][colunas];
+        if (linhas < 1 || colunas < 1) {
+            throw new BoardException("Erro ao criar o tabuleiro: É necessário que existe ao menos uma linha e uma coluna.");
+        }
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pieces = new Piece[linhas][colunas];
     }
 
     public int getLinhas() {
         return linhas;
     }
 
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
-
     public int getColunas() {
         return colunas;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
-
-    public Piece peca(int linha, int coluna) {
+    public Piece piece(int linha, int coluna) {
+        if (!positionExists(linha, coluna)) {
+            throw new BoardException("Posição não presente no tabuleiro");
+        }
         return pieces[linha][coluna];
     }
-    public Piece peca(Position position) {
+    public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Posição não presente no tabuleiro");
+        }
         return pieces[position.getLinha()][position.getColuna()];
     }
     public void placePiece (Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("Já existe uma peça nessa posição " + position);
+        }
         pieces[position.getLinha()][position.getColuna()] = piece;
         piece.posicao = position;
     }
-
-
+    private boolean positionExists(int linha, int coluna) {
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+    }
+    public boolean positionExists (Position position) {
+        return positionExists(position.getLinha(), position.getColuna());
+    }
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Posição não presente no tabuleiro");
+        }
+        return piece(position) != null;
+    }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -19,7 +19,7 @@ public class ChessMatch {
         ChessPiece[][] mat = new ChessPiece[tabuleiro.getLinhas()][tabuleiro.getColunas()];
         for (int i = 0; i < tabuleiro.getLinhas(); i++) {
             for (int j = 0; j< tabuleiro.getColunas(); j++) {
-                mat[i][j] = (ChessPiece) tabuleiro.peca(i, j);
+                mat[i][j] = (ChessPiece) tabuleiro.piece(i, j);
             }
         }
         return mat;
